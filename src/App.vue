2df75f1cc8ca2db114603f421fa161b510fbe395
917017420 <917017420@qq.com>
2020-07-08 15:04:43 +0800

    v1.0

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Home/>
+    </div>
+  </div>
+</template>
+<script>
+// eslint-disable-next-line no-unused-vars
+import Home from './views/Home'
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 0
+  width 100%
+body
+  margin 0
+</style>
